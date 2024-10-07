5ee75939e9eb003c08349dd484db44da09d12b57
Mi270707 <114525721+Mi270707@users.noreply.github.com>
2024-10-07 10:28:30 -0300

    .

--- Graficos/quantidadeUsuarios.js
+++ Graficos/quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS, tickConfig} from "./common.js"
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -9,14 +11,50 @@ async function quantidadeUsuarios() {
         {
             x: nomeDasRedes,
             y: quantidadeUsuarios,
-            type:'bar'
+            type:'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
     ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor:getCSS('--bg-color')
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x=0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes socias',
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secundary-color')
+                }
+            }
+        }
+
+    }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newplot(grafico, data)
+    Plotly.newplot(grafico, data, layout)
 }
 
 quantidadeUsuarios()
