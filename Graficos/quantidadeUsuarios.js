f9d1d11f1cb2089491988cdbb0eee1b4168e0072
Mi270707 <114525721+Mi270707@users.noreply.github.com>
2024-11-04 09:56:12 -0300

    Update quantidadeUsuarios.js

--- Graficos/quantidadeUsuarios.js
+++ Graficos/quantidadeUsuarios.js
@@ -1,16 +1,16 @@
-import { getCSS, tickConfig} from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
 
     const data = [
         {
             x: nomeDasRedes,
-            y: quantidadeUsuarios,
+            y: quantidadeDeUsuarios,
             type: 'bar',
             marker: {
                 color: getCSS('--primary-color')
